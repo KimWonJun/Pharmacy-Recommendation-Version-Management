f78f5c6c144d94bb3d73c20018ba37f24a9b587e
kimwonjun <kwj0313im@naver.com>
2023-04-24 23:39:17 +0900

    feat: kakao api 테스트 추가
    주소 search api 테스트 추가

--- src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -8,7 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired
 class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
     @Autowired
-    private PharmacyRepository pharmacyRepository;
+    private PharmacyRepository pharmacyRepository
+
+    def setup() {
+        pharmacyRepository.deleteAll()
+    }
 
     @Test
     def "Pharmacy Repostiory save"() {
@@ -33,5 +37,27 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
         result.getPharmacyName() == name
         result.getLatitude() == latitude
         result.getLongitude() == longitude
+    }
+
+    def "PharmacyRepository saveAll"() {
+        given:
+        String address = "서울 특별시 성북구 중앙동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build();
+
+        when:
+        pharmacyRepository.saveAll(Arrays.asList(pharmacy))
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.size() == 1
     }
 }
