712b18da02c292b4bd1939976f2cca1df21c9c71
kimwonjun <kwj0313im@naver.com>
2023-06-07 00:10:13 +0900

    feat: Test code 추가
    base62 encode/decode test

--- src/main/java/com/example/project/direction/service/DirectionService.java
+++ src/main/java/com/example/project/direction/service/DirectionService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.transaction.Transactional;
 import java.util.Collections;
@@ -25,6 +26,7 @@ public class DirectionService {
 
     private static final int MAX_SEARCH_COUNT = 3;  // 약국 최대 검색 갯수
     private static final double RADIUS_KM = 10.0;   // 반경 10 km
+    private static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
 
     private final PharmacySearchService pharmacySearchService;
     private final DirectionRepository directionRepository;
@@ -39,9 +41,17 @@ public class DirectionService {
         return directionRepository.saveAll(directionList);
     }
 
-    public Direction findById(String encodedId) {
+    public String findDirectionUrlById(String encodedId) {
         Long decodedId = base62Service.decodeDirectionId(encodedId);
-        return directionRepository.findById(decodedId).orElse(null);
+        Direction direction = directionRepository.findById(decodedId).orElse(null);
+
+        String params = String.join(",", direction.getTargetPharmacyName(),
+            String.valueOf(direction.getTargetLatitude()), String.valueOf(direction.getTargetLongitude()));
+
+        String result = UriComponentsBuilder.fromHttpUrl(DIRECTION_BASE_URL + params)
+            .toUriString();
+
+        return result;
     }
 
     public List<Direction> buildDirectionList(DocumentDto documentDto) {
